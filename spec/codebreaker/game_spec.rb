4be5048e144aeacf0bbb0f95bea38600c91afeb7
David Smyth <david.smyth@millennium-space.com>
2014-08-27 19:37:51 -0700

    refactor game_spec DRY

--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -3,20 +3,18 @@ require 'spec_helper'
 module Codebreaker
   describe Game do
     describe "#start" do
-      it "sends a welcome message" do
-        # Use RSpec dynamic test double framework RSpec::Mocks
-        # to create a dynamic test double. The "as_null_object"
-        # pattern means to ignore any other methods and arguments
-        # other than the one specified to be expected. Yet another
-        # lousy design pattern name, but OK, the pattern is a good one.
-        output = double('output').as_null_object
-        game = Game.new(output)
+      # Use RSpec dynamic test double framework RSpec::Mocks
+      # to create a dynamic test double. The "as_null_object"
+      # pattern means to ignore any other methods and arguments
+      # other than the one specified to be expected. Yet another
+      # lousy design pattern name, but OK, the pattern is a good one.
+      let(:output) { double('output').as_null_object }
+      let(:game)   { Game.new(output) }
+      it "sends a welcome message" do        
         expect(output).to receive(:puts).with('Welcome to Codebreaker!')
         game.start
       end
       it "prompts for first guess" do
-        output = double('output').as_null_object
-        game = Game.new(output)
         expect(output).to receive(:puts).with('Enter guess:')
         game.start
       end
